b7d226bf5afdc22136cecf47e0db464c211b74d7
KarlaSaenz <karli.saenz@gmail.com>
2021-08-11 20:23:03 -0300

    prueba 2

--- App.js
+++ App.js
@@ -18,7 +18,6 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  console.log("Eee")
   render() {
     return (
       <View style={styles.container}>
